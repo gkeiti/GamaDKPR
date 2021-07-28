162689d47f50a0893112d09b2c330c3a925b3da6
renan <renan.schotten@gmail.com>
2021-07-27 22:09:22 -0300

    iniciando projeto

---
+++ android/app/src/main/kotlin/com/example/trabalho_final_dgpr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trabalho_final_dgpr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/app_constants/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color cyan = Color(0xFF44C2FD);
+  static const Color purple = Color(0XFF343090);
+  static const Color green = Color(0XFF58B368);
+  static const Color pink = Color(0XFFF5487F);
+  static const Color yellow = Color(0XFFFAC736);
+
+  static const linearGradient = LinearGradient(
+    colors: <Color>[
+      cyan,
+      purple,
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    stops: [
+      0.1,
+      0.7,
+    ],
+    transform: GradientRotation((-34 * 3.141592) / 180),
+  );
+}
