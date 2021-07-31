dbbd5407408beabab36671b851263b4db5f82994
Diogo Saimon Alves dos Santos <diogosaimon@hotmail.com>
2021-07-30 23:10:19 -0300

    Adicionado uma AppBar com fundo branco, ajustados os positioned nos widget do body

--- lib/features/register/register_name_email/register_name_email.dart
+++ lib/features/register/register_name_email/register_name_email.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:trabalho_final_dgpr/shared/app_constants/app_colors.dart';
 import 'package:trabalho_final_dgpr/shared/app_constants/text_styles.dart';
@@ -17,22 +18,29 @@ class RegisterNameEmailPage extends StatefulWidget {
 class _RegisterNameEmailPageState extends State<RegisterNameEmailPage> {
   TextEditingController? _nameController = TextEditingController();
   TextEditingController? _emailController = TextEditingController();
- 
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
+      appBar: AppBar(
+        backwardsCompatibility: false,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.dark,
+          statusBarColor: Colors.transparent,
+        ),
+        backgroundColor: AppColors.white,
+        elevation: 0.0,
+      ),
       body: Stack(
         children: [
           Positioned(
-            top: 74.0,
+            top: 0.0,
             left: 48.0,
             child: LogoBudgetWidget(),
           ),
           Positioned(
-            top: 144.0,
+            top: 70.0,
             left: 48.0,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
@@ -46,32 +54,26 @@ class _RegisterNameEmailPageState extends State<RegisterNameEmailPage> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(bottom: 144.0),
+            padding: EdgeInsets.only(top: 144.0, left: 48.0, right: 49.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 48.0),
-                  child: InputText(
-                    autofocus: true,
-                    controller: _nameController,
-                    onChanged: (value) => _nameController?.value,
-                    label: 'Nome',
-                    textInputType: TextInputType.name,
-                    validator: (String? value) {},
-                  ),
+                InputText(
+                  autofocus: false,
+                  controller: _nameController,
+                  onChanged: (value) => _nameController?.value,
+                  label: 'Nome',
+                  textInputType: TextInputType.name,
+                  validator: (String? value) {},
                 ),
                 SizedBox(height: 32.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 48.0),
-                  child: InputText(
-                    autofocus: false,
-                    controller: _emailController,
-                    onChanged: (value) => _emailController?.value,
-                    label: 'E-mail',
-                    textInputType: TextInputType.emailAddress,
-                    validator: (String? value) {},
-                  ),
+                InputText(
+                  autofocus: false,
+                  controller: _emailController,
+                  onChanged: (value) => _emailController?.value,
+                  label: 'E-mail',
+                  textInputType: TextInputType.emailAddress,
+                  validator: (String? value) {},
                 ),
               ],
             ),
