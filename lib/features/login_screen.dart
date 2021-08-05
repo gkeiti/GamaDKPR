7c4d2f7d7db02512afc5f727bcd3add933855c97
Patrick Bahia <patrickfbahia@hotmail.com>
2021-08-05 17:23:19 -0300

    tirei os comentarios

--- lib/features/login_screen.dart
+++ lib/features/login_screen.dart
@@ -84,7 +84,6 @@ class _LoginScreenState extends State<LoginScreen> {
                 padding: const EdgeInsets.only(
                   top: 16.0,
                   left: 198,
-                  // right: 48,
                 ),
                 child: ContinueButton(
                   onPressed: () {},
@@ -102,7 +101,6 @@ class _LoginScreenState extends State<LoginScreen> {
                 height: 36,
                 decoration: BoxDecoration(
                   border: Border.all(color: AppColors.greyGoogle),
-                  // color: Colors.blue,
                   borderRadius: BorderRadius.circular(20),
                 ),
                 child: SizedBox.expand(
@@ -112,23 +110,6 @@ class _LoginScreenState extends State<LoginScreen> {
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Image.asset('assets/images/continue_google.png'),
-                        // Icon(Icons.ac_unit),
-                        // // MaterialButton(
-                        //   // onPressed: () {},
-                        //   // child:
-                        //   Text(
-                        //     'CONTINUAR COM O GOOGLE',
-                        //     style: TextStyle(
-                        //       fontWeight: FontWeight.w500,
-                        //       // botei weight de 500 pq é como ta mandando no figma, mas acho que o 600 ta melhor
-                        //       letterSpacing: 0.46,
-                        //       fontSize: 13,
-                        //       fontStyle: FontStyle.normal,
-                        //       fontFamily: 'Roboto',
-                        //       color: AppColors.black54,
-                        //     ),
-                        //   ),
-                        // ),
                       ],
                     ),
                   ),
@@ -165,12 +146,3 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
-
-
-// Tirar a Row do icone do google caso deixe com a imagem mesmo
-// eu tava fazendo na mao da forma que eles deixaram no figma, mas tava muito
-// diferente, colocando a imagem do icone foi o mais próximo que ficou 
-// pode só descomentar o código que ta comentado e comentar 
-// a imagem, o sized box e o material button pra ver como ficou
-// ta com aquele icone meio aleatorio pq eu tava testando como ficaria pra não baixar
-// o do google pra nada
