aceaf5747626108b07eae822e669b8a7b9703b42
Patrick Bahia <patrickfbahia@hotmail.com>
2021-08-05 22:02:17 -0300

    botoes sem bug

--- lib/modules/home/pages/name_email_screen.dart
+++ lib/modules/home/pages/name_email_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:trabalho_final_dgpr/shared/app_constants/app_colors.dart';
 import 'package:trabalho_final_dgpr/shared/app_constants/text_styles.dart';
 import 'package:trabalho_final_dgpr/shared/widgets/continue_button.dart';
 import 'package:trabalho_final_dgpr/shared/widgets/input_text.dart';
@@ -15,86 +14,88 @@ class _NameEmailState extends State<NameEmail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(48, 74, 48, 0),
         child: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding: const EdgeInsets.only(
-                  top: 74,
-                  right: 212,
-                  left: 48,
-                ),
+                padding: const EdgeInsets.only(bottom: 16.0),
                 child: Image.asset('assets/images/budget_logo_black.png'),
               ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 16.15,
-                  left: 48,
-                  right: 112,
-                ),
-                child: Container(
-                  child: Text(
-                    'Bem-vindo!',
-                    style: TextStyles.cyan48w400Roboto,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 8,
-                  right: 57,
-                  bottom: 130,
-                ),
-                child: Text(
-                  'Por favor insira seus dados\nnos campos abaixo.',
-                  style: TextStyles.purple20w600Roboto,
+              Container(
+                padding: EdgeInsets.only(bottom: 112),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Bem-vindo!',
+                      style: TextStyles.cyan48w400Roboto,
+                    ),
+                    Text(
+                      'Por favor insira seus dados\nnos campos abaixo.',
+                      style: TextStyles.purple20w600Roboto,
+                    ),
+                  ],
                 ),
               ),
               Container(
-                padding: EdgeInsets.only(left: 48, right: 48, bottom: 50),
-                child: InputText(
-                  label: 'Nome',
-                  obscureText: false,
-                  textInputType: TextInputType.text,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 48),
-                child: InputText(
-                  label: 'Insira seu e-mail',
-                  obscureText: false,
-                  textInputType: TextInputType.emailAddress,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 50),
+                      child: InputText(
+                        label: 'Nome',
+                        obscureText: false,
+                        textInputType: TextInputType.text,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 0),
+                      child: InputText(
+                        label: 'Insira seu e-mail',
+                        obscureText: false,
+                        textInputType: TextInputType.emailAddress,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
         ),
       ),
-      bottomSheet: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          TextButton(
-            onPressed: () {
-              Navigator.popAndPushNamed(context, '/login');
-            },
-            child: Text(
-              'VOLTAR',
-              style: TextStyles.grey14w500Roboto,
-            ),
+      bottomSheet: Visibility(
+        visible: MediaQuery.of(context).viewInsets.bottom == 0,
+        child: Positioned(
+          bottom: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.popAndPushNamed(context, '/login');
+                },
+                child: Text(
+                  'VOLTAR',
+                  style: TextStyles.grey14w500Roboto,
+                ),
+              ),
+              Text(
+                '1/4',
+                style: TextStyles.black14w400Robotoblack,
+              ),
+              ContinueButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/tel_cpf');
+                },
+              ),
+            ],
           ),
-          Text(
-            '1/4',
-            style: TextStyles.black14w400Robotoblack,
-          ),
-          ContinueButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/tel_cpf');
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
 }
-// precisa ajeitar o posicionamento dos botoes
